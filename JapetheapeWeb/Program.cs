a2cab6dd6b493cda70c7b52ef7eecd6b1061af87
Jonathan Paugh <j.d.paugh@gmail.com>
2021-11-27 09:10:33 -0800

    Web Server -Created WebServer service that can be used to serve web content to clients -Updated the way ConsoleProgram parses args, OnSetup() is used to setup arg values, OnStart() is called after to start the program -ConsoleProgram can now start synchronously or asynchronously with Run() and RunAsync() -Logger class created to create independent log files -Log now creates a Logger for default logging

---
+++ JapetheapeWeb/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using JapeCore;
+using JapeService;
+
+namespace JapeWeb
+{
+    internal class Program : ConsoleProgram<int, int>
+    {
+        protected override string DefaultLog => "server.log";
+
+        private int http;
+        private int https;
+
+        private static async Task Main(string[] args) => await Run<Program>(args);
+
+        protected override IEnumerable<ICommandArg> Args() => Service.Args;
+
+        protected override void OnSetup(int http, int https)
+        {
+            this.http = http;
+            this.https = https;
+        }
+
+        protected override void OnStart()
+        {
+            WebServer webServer = new(http, https);
+            webServer.Start();
+            WaitShutdown();
+        }
+    }
+}
---
+++ JapetheapeWeb/WebServer.cs
@@ -0,0 +1,37 @@
+﻿using JapeCore;
+using JapeHttp;
+using JapeService;
+using Microsoft.Extensions.Logging;
+
+namespace JapeWeb
+{
+    public class WebServer : Service
+    {
+        protected override string StartString => $"{base.StartString}: {http}";
+
+        private readonly int http;
+        private readonly int https;
+
+        private Logger accessLogger = Log.Create("access.log");
+
+        public WebServer(int http, int https) : base(http, https)
+        {
+            this.http = http;
+            this.https = https;
+        }
+
+        protected override Listener ServiceListener()
+        {
+            WebListener listener = new();
+
+            #pragma warning disable 1998
+            listener.Use(async (context) => 
+            {
+                accessLogger.Write($"({context.Connection.RemoteIpAddress.MapToIPv4()}) => Request: {context.Request.Path}");
+            });
+            #pragma warning restore 1998
+
+            return listener;
+        }
+    }
+}
