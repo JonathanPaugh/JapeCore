d0749b0b0f8cc3aed556d450b67f842531715e16
Jonathan Paugh <j.d.paugh@gmail.com>
2021-11-28 23:22:24 -0800

    Project File Updates

--- JapeWeb/Program.cs
+++ JapeWeb/Program.cs
@@ -1,9 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Threading;
+﻿using System.Collections.Generic;
 using System.Threading.Tasks;
 using JapeCore;
-using JapeCore.Library;
 using JapeService;
 
 namespace JapeWeb
