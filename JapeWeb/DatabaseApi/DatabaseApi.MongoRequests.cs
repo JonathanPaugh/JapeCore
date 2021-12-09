b52e1577e7ecc2c981473a6dd9dc78ddf234e923
Jonathan Paugh <j.d.paugh@gmail.com>
2021-12-08 21:20:10 -0800

    Reorganize -Restructured some directories -Fixed parameters for some DatabaseApi requests

--- JapeWeb/DatabaseApi/DatabaseApi.MongoRequests.cs
+++ JapeWeb/DatabaseApi/DatabaseApi.MongoRequests.cs
@@ -14,6 +14,16 @@ namespace JapeWeb
             return Request(mongoData);
         }
 
+        public string MongoGet(string database, string collection, string id)
+        {
+            return MongoRequest("get", new JsonData
+            {
+                { "store", database },
+                { "collection", collection },
+                { "key", id }
+            }).Read();
+        }
+
         /// <summary>
         /// Inserts a data into a collection.
         /// </summary>
@@ -28,6 +38,38 @@ namespace JapeWeb
                 { "store", database },
                 { "collection", collection },
                 { "data", data }
+            }).Read();
+        }
+
+        public string MongoUpdate(string database, string collection, string id, JsonData data)
+        {
+            return MongoRequest("update", new JsonData
+            {
+                { "store", database },
+                { "collection", collection },
+                { "key", id },
+                { "data", data }
+            }).Read();
+        }
+
+        public string MongoRemove(string database, string collection, string id, string[] data)
+        {
+            return MongoRequest("remove", new JsonData
+            {
+                { "store", database },
+                { "collection", collection },
+                { "key", id },
+                { "data", data }
+            }).Read();
+        }
+
+        public string MongoDelete(string database, string collection, string id)
+        {
+            return MongoRequest("delete", new JsonData
+            {
+                { "store", database },
+                { "collection", collection },
+                { "key", id }
             }).Read();
         }
     }
